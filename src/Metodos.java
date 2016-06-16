70b12aaf27e8ff6b3134ffa6f0583f397a44f8c9
JP <JP@192.168.0.14>
2016-06-16 17:35:44 +0200

    Añadimos los metodos buscar tema y buscarComentarios

--- src/Metodos.java
+++ src/Metodos.java
@@ -1,8 +1,11 @@
 
+import facebook4j.Comment;
 import facebook4j.Facebook;
 import facebook4j.FacebookException;
 import facebook4j.FacebookFactory;
+import facebook4j.Post;
 import facebook4j.PostUpdate;
+import facebook4j.ResponseList;
 import facebook4j.conf.ConfigurationBuilder;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -45,12 +48,23 @@ public class Metodos {
         PFclase.likePost(JOptionPane.showInputDialog("Introduce la ID del Post \n (Para obtenerla haz click en la hora \n del post y copia los numeros del enlace)"));
     }
 
-    public void postImagen() throws MalformedURLException, FacebookException {
+    public void subirImagen() throws MalformedURLException, FacebookException {
 
         PostUpdate post = new PostUpdate(new URL("https://www.formula1.com/content/fom-website/en.html"))
                 .picture(new URL("http://e01-marca.uecdn.es/assets/multimedia/imagenes/2016/03/20/14584597103222.jpg"))
                 .description("Crash Fernando Alonso ");
         PFclase.postFeed(post);
 
+    }
+    public void buscarTema() throws FacebookException {
+        //No lo admite GraphAPI 2.0 o superior
+        ResponseList<Post> results = PFclase.searchPosts(JOptionPane.showInputDialog("Selecciona el tema del que buscar posts"));
+        JOptionPane.showMessageDialog(null, results);
+    }
+
+    public void comentarios() throws FacebookException {
+
+        ResponseList<Comment> lista = PFclase.getCommentReplies(JOptionPane.showInputDialog("Selecciona el post del que buscar comentarios"));
+        System.out.println(lista);
     }
 }
