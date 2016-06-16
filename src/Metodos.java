39a9f8f3ae38dff344087b698c2d3fee53e2cf69
JP <JP@192.168.0.14>
2016-06-16 17:56:27 +0200

    commit final y javadoc realizado,metodos funcionando

--- src/Metodos.java
+++ src/Metodos.java
@@ -23,8 +23,15 @@ import javax.swing.JOptionPane;
  */
 public class Metodos {
 
+    /**
+     *
+     */
     public Facebook PFclase;
 
+    /**
+     *Metodo donde se insertan los tokens de nuestra app para que funcione,en el AccessToken hay que configurar los permisos
+     * @return
+     */
     public Facebook iniciar() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
         //introducir los tokens en los huecos con comillas
@@ -38,17 +45,30 @@ public class Metodos {
         return PFclase;
     }
 
+    /**
+     *Metodo que nos actualiza el estado,donde nosotros introduciremos el estado que queramos
+     * @throws FacebookException
+     */
     public void estado() throws FacebookException {
 
         PFclase.postStatusMessage(JOptionPane.showInputDialog("Introduce estado"));
 
     }
 
+    /**
+     *Metodo que sirve para dar like a un post que nosotros querramos donde nosotros tendremos que expecificar el ID
+     * @throws FacebookException
+     */
     public void darLike() throws FacebookException {
 
         PFclase.likePost(JOptionPane.showInputDialog("Introduce la ID del Post \n (Para obtenerla haz click en la hora \n del post y copia los numeros del enlace)"));
     }
 
+    /**
+     *Metodo para postear una imagen,tendremos que ponerle la direccion manualmente
+     * @throws MalformedURLException
+     * @throws FacebookException
+     */
     public void subirImagen() throws MalformedURLException, FacebookException {
 
         PostUpdate post = new PostUpdate(new URL("https://www.formula1.com/content/fom-website/en.html"))
@@ -57,23 +77,40 @@ public class Metodos {
         PFclase.postFeed(post);
 
     }
+
+    /**
+     *Metodo para buscar un tema pero no funciona a partir de la GraphAPI 2.0 o superior
+     * @throws FacebookException
+     */
     public void buscarTema() throws FacebookException {
-        //No lo admite GraphAPI 2.0 o superior
+       
         ResponseList<Post> results = PFclase.searchPosts(JOptionPane.showInputDialog("Selecciona el tema del que buscar posts"));
         JOptionPane.showMessageDialog(null, results);
     }
 
+    /**
+     *Metodo para buscar comentarios de un post que nosotros indiquemos mediante el ID
+     * @throws FacebookException
+     */
     public void buscarComentarios() throws FacebookException {
 
         ResponseList<Comment> lista = PFclase.getCommentReplies(JOptionPane.showInputDialog("Selecciona el post del que buscar comentarios"));
         System.out.println(lista);
     }
     
+    /**
+     *Metodo para comentar en una foto mediante el ID de la foto que nosotros introduzcamos,en mi caso no da error pero no publica
+     * @throws FacebookException
+     */
     public void comentarFoto() throws FacebookException {
 
         PFclase.commentPhoto(JOptionPane.showInputDialog("Introduce la ID del post \n (Para obtenerla haz click en la hora \n del post y copia los numeros del enlace)"), JOptionPane.showInputDialog("Introduce el comentario"));
     }
 
+    /**
+     *Metodo para buscar un usuario,introducimos un nombre y nos mostrara todos los usuarios con dicho nombre
+     * @throws FacebookException
+     */
     public void buscarUsuario() throws FacebookException {
 
         ResponseList<User> userlist = PFclase.searchUsers(JOptionPane.showInputDialog("Introduce el nombre a buscar"));
